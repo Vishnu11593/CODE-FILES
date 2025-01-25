83c46fa6572ef56bab9dfc816a53ac091045690e
Y vishnu vardhan <vishnuvardhanyeduresi@gmail.com>
2025-01-25 10:42:14 +0530

    Add files via upload

---
+++ List, tuple and Dictionary.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Create a list
+my_list = [10, 20, 30, 40, 50]
+print(my_list[0])  
+print(my_list[2])  
+print(my_list[-1]) 
+
+
+# In[4]:
+
+
+# Create a tuple
+my_tuple = ('apple', 'banana', 'cherry', 'date', 'elderberry')
+print(my_tuple[1])  
+print(my_tuple[3])  
+print(my_tuple[-2]) 
+
+
+# In[6]:
+
+
+# Create a dictionary
+my_dict = {
+    'name': 'Alice',
+    'age': 25,
+    'city': 'New York',
+    'country': 'USA',
+    'hobby': 'painting'
+}
+print(my_dict['name'])   
+print(my_dict['city'])   
+print(my_dict['hobby'])  
+
+
+# In[7]:
+
+
+# Accessing elements in different structures
+print(my_list[1])           
+print(my_tuple[4])          
+print(my_dict['country'])   
+
+
+# In[ ]:
+
+
+
+
