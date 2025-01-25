ba93768b05328e4c4152f184f61d2b710709ceab
Y vishnu vardhan <vishnuvardhanyeduresi@gmail.com>
2025-01-25 10:32:02 +0530

    Add files via upload

---
+++ declare variables of different data types.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#declare variables of different data types
+
+
+# In[3]:
+
+
+integer_var = 12         
+float_var = 7.14          
+string_var = "Hello, World!" 
+boolean_var = True         
+
+
+print("Integer variable:", integer_var, "Type:", type(integer_var))
+print("Float variable:", float_var, "Type:", type(float_var))
+print("String variable:", string_var, "Type:", type(string_var))
+print("Boolean variable:", boolean_var, "Type:", type(boolean_var))
+
+
+# In[ ]:
+
+
+
+
